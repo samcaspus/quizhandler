9f1fc8cd584a13ff320319af592b11f2d14aa9b5
samcaspus <sandeepguptan1998@gmail.com>
2020-12-27 19:14:57 +0530

    creation of base setup

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require(`express`);
+const router = express.Router();
+const port = 8000;
+
+app = express();
+
+app.use("/", require("./router/index"))
+
+
+
+
+app.listen(port, (err) => {
+
+    if (err) {
+        console.log("something went wrong");
+    }
+
+    console.log("established connection with port");
+
+})
---
+++ router/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+
+router.get("/progress")
+router.get("/question")
+router.post("/create-question")
+router.post("/delete-auestion")
+
+
+
+module.exports = router;
